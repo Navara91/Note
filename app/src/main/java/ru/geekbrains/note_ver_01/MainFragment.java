3eb2bc99637459e02d5cb92e0cb491a546c88a94
Nikolay <kuoluska@mail.ru>
2021-07-19 00:34:56 +0900

    создал фрагмент, набросил визуалку с 2мя кнопками, начал создавать логику заполнения фрагмента

---
+++ app/src/main/java/ru/geekbrains/note_ver_01/MainFragment.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.note_ver_01;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    // вызывается после создания макета фрагмента, здесь мы проинициализируем список
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+
+}
